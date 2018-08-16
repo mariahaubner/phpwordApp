83b2fe7e9d7f253c12b23afe697d1b5d3bed5bc9
Maria Haubner <maria.haubner@mayflower.de>
2018-08-16 19:57:47 +0200

    [WIP] add xPath merging

--- src/xPath/export.php
+++ src/xPath/export.php
@@ -1,58 +1,63 @@
 <?php
 include_once '../includes.php';
 
-use PhpOffice\PhpWord\PhpWord;
-use PhpOffice\PhpWord\IOFactory;
-
 if (isset($_POST['content']) && $_POST['content'] !== '') {
     export();
 } else {
-    var_dump($_POST);
-    #error();
+    error();
 }
 
 function export()
 {
-    $templateFile  = "MergeTemplate.docx";
-    $generatedFile = "MyDocument.docx";
-    $targetFile    = "MergeResult.docx";
+    # TODO: Use genereated file insted of static - use content input
+    $generatedFile = WORD_FILES . "Tmp.docx";
+    $templateFile = WORD_FILES . "XPathTemplateOrange.docx";
 
-// copy template to target
+    if ($_POST['templateChoice'] === 'blue') {
+        $templateFile = WORD_FILES . "XPathTemplateBlue.docx";
+    }
+
+    $targetFileName = "XPathResult.docx";
+    $targetFile = WORD_EXPORTS . $targetFileName;
+
+    // copy template to target
     copy($templateFile, $targetFile);
 
-// open target
+    // open target
     $targetZip = new \ZipArchive();
     $targetZip->open($targetFile);
     $targetDocument = $targetZip->getFromName('word/document.xml');
-    $targetDom      = new DOMDocument();
+    $targetDom = new DOMDocument();
     $targetDom->loadXML($targetDocument);
     $targetXPath = new \DOMXPath($targetDom);
     $targetXPath->registerNamespace("w", "http://schemas.openxmlformats.org/wordprocessingml/2006/main");
 
-// open source
+    // open source
     $sourceZip = new \ZipArchive();
     $sourceZip->open($generatedFile);
     $sourceDocument = $sourceZip->getFromName('word/document.xml');
-    $sourceDom      = new DOMDocument();
+    $sourceDom = new DOMDocument();
     $sourceDom->loadXML($sourceDocument);
     $sourceXPath = new \DOMXPath($sourceDom);
     $sourceXPath->registerNamespace("w", "http://schemas.openxmlformats.org/wordprocessingml/2006/main");
 
-    /** @var DOMNode $replacementMarkerNode node containing the replacement marker $CONTENT$ */
-    $replacementMarkerNode = $targetXPath->query('//w:p[contains(translate(normalize-space(), " ", ""),"$CONTENT$")]')[0];
+    /** @var DOMNode $placeholderNode node containing the replacement marker PLACEHOLDER$ */
+    $placeholderNode = $targetXPath->query('//w:p[contains(translate(normalize-space(), " ", ""),"PLACEHOLDER")]')[0];
 
-// insert source nodes before the replacement marker
+    // insert source nodes before the replacement marker
     $sourceNodes = $sourceXPath->query('//w:document/w:body/*[not(self::w:sectPr)]');
 
     foreach ($sourceNodes as $sourceNode) {
-        $imported = $replacementMarkerNode->ownerDocument->importNode($sourceNode, true);
-        $inserted = $replacementMarkerNode->parentNode->insertBefore($imported, $replacementMarkerNode);
+        $imported = $placeholderNode->ownerDocument->importNode($sourceNode, true);
+        $inserted = $placeholderNode->parentNode->insertBefore($imported, $placeholderNode);
     }
 
-// remove $replacementMarkerNode from the target DOM
-    $replacementMarkerNode->parentNode->removeChild($replacementMarkerNode);
+    // remove $placeholderNode from the target DOM
+    $placeholderNode->parentNode->removeChild($placeholderNode);
 
-// save target
+    // save target
     $targetZip->addFromString('word/document.xml', $targetDom->saveXML());
     $targetZip->close();
+
+    download($targetFileName);
 }
